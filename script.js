15012b65034b91bbf12dd388cfe32fbdc633fbfe
Lubov Voronova <lubov.voronova88@gmail.com>
2020-03-03 14:30:28 +0300

    dropdown menu

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict"
-console.log("ooo");
+
+// MENU-DROPDOWN
+document.addEventListener('click', function(event) {
+    let target = event.target; 
+    let subMenuList = document.querySelectorAll('ul.sub-menu');    
+    if (target.closest('.menu-item')) {
+        closeMenu();
+        target.querySelector('ul.sub-menu').style.visibility = "visible";
+        } else {
+            closeMenu();
+        }
+    function closeMenu() {
+        for (let i = 0; i < subMenuList.length; i++) {
+            subMenuList[i].style.visibility = "hidden";
+        }
+    }    
+});
