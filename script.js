c58a29041b7ccbe93306f2ee1fe4fa3710592a49
Lubov Voronova <lubov.voronova88@gmail.com>
2020-03-04 16:06:04 +0300

    weather widget and social media links and added files for navigation

--- script.js
+++ script.js
@@ -16,3 +16,49 @@ document.addEventListener('click', function(event) {
         }
     }    
 });
+
+// WEATHER 
+let weatherRequest = new XMLHttpRequest();
+let url = new URL('http://api.openweathermap.org/data/2.5/weather');
+url.searchParams.set('lat', '59.93');
+url.searchParams.set('lon', '30.31');
+url.searchParams.set('appid', '8531afc05368e7ed4171872b5b9c8b7f');
+url.searchParams.set('units', 'metric');
+url.searchParams.set('lang', 'ru');
+weatherRequest.open('GET', url, true);
+weatherRequest.send();
+
+let weatherBlock = document.querySelector('.weather');
+
+weatherRequest.onload = function() {    
+    let response = JSON.parse(this.response);
+    let weatherData = response.main;
+
+    //Icon
+    let weatherInfo = response.weather[0];
+    let iconData = weatherInfo.icon;
+    let icon = document.createElement('img');
+    icon.src = `http://openweathermap.org/img/wn/${iconData}.png`;
+    weatherBlock.appendChild(icon);
+
+    //Temperature
+    let temperatureData =  weatherData.temp;
+    temperatureData = Math.round(temperatureData);
+    let temp = document.createElement('p');
+    if (temperatureData > 0) {
+        temp.textContent = " + " + temperatureData + " ℃";
+    } else {
+        temp.textContent = temperatureData + " ℃";
+    }         
+    temp.classList.add('degrees'); 
+    weatherBlock.appendChild(temp);
+    
+    //Description
+    let descriptionData = weatherInfo.description;
+    createWeatherElement(descriptionData, 'p');
+}
+function createWeatherElement(element, tagName) {
+    let newElement = document.createElement(tagName);
+    newElement.textContent = element;
+    weatherBlock.appendChild(newElement);
+}
