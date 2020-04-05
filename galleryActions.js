f42e5bef7542dbac1110e3771b79ef047f449974
Lubov Voronova <lubov.voronova88@gmail.com>
2020-04-05 12:22:33 +0300

    clock

--- galleryActions.js
+++ galleryActions.js
@@ -14,40 +14,50 @@ function getPhotos(searchWord) {
         console.log("Ошибка HTTP: " + response.status);
         }        
     })
-    .then(function(myPictures) {   
-        let Bigphoto = photoContainer.querySelector('.big-photo');      
-        let bigImage = document.createElement('img');
-        bigImage.classList.add('big-picture');   
-        bigImage.src = myPictures.results[9].urls.regular;
-        bigImage.alt = myPictures.results[9].alt_description;
-        Bigphoto.append(bigImage);
-        
-        let smallPhotoCount = Math.floor(document.documentElement.clientWidth / 200);        
-        
-        for (let i=0; i < smallPhotoCount; i++) {
-            let smallImage = document.createElement('img');
-            smallImage.src = myPictures.results[i].urls.thumb;
-            smallImage.alt = myPictures.results[i].alt_description;
-            smallImage.classList.add('small');            
-            smallPhotoContainer.append(smallImage);
-        }  
-        
-        let smallPicures = document.querySelectorAll('.small');        
-        for (let i = 0; i < smallPicures.length; i++) {
-            smallPicures[i].onclick = function() {
-                bigImage.src = myPictures.results[i].urls.regular;
-                bigImage.alt = myPictures.results[i].alt_description;
+    .then(function(myPictures) { 
+        if (myPictures.total == '0') {
+            let errorMessage = document.createElement('div');
+            errorMessage.classList.add('error-message');
+            errorMessage.textContent = "Ничего не найдено";
+            photoContainer.append(errorMessage);
+        }  else {            
+            let Bigphoto = photoContainer.querySelector('.big-photo');      
+            let bigImage = document.createElement('img');
+            bigImage.classList.add('big-picture');   
+            bigImage.src = myPictures.results[9].urls.regular;
+            bigImage.alt = myPictures.results[9].alt_description;
+            Bigphoto.append(bigImage);
+            
+            let smallPhotoCount = Math.floor(document.documentElement.clientWidth / 200);        
+            
+            for (let i=0; i < smallPhotoCount; i++) {
+                let smallImage = document.createElement('img');
+                smallImage.src = myPictures.results[i].urls.thumb;
+                smallImage.alt = myPictures.results[i].alt_description;
+                smallImage.classList.add('small');            
+                smallPhotoContainer.append(smallImage);
             }  
+            
+            let smallPicures = document.querySelectorAll('.small');
+            for (let i = 0; i < smallPicures.length; i++) {
+                smallPicures[i].onclick = function() {
+                    bigImage.src = myPictures.results[i].urls.regular;
+                    bigImage.alt = myPictures.results[i].alt_description;
+                }  
+            }
         }
     });
 }
 
+
 function removePhotos() {
-    photoContainer.querySelector('.big-picture').remove();
     let smallPicures = document.querySelectorAll('.small');
-    for (let i = 0; i < smallPicures.length; i++) {
-        smallPicures[i].remove();
-    }
+    if (smallPicures.length != 0) {
+        photoContainer.querySelector('.big-picture').remove();    
+        for (let i = 0; i < smallPicures.length; i++) {
+            smallPicures[i].remove();
+        }
+    }    
 }
 
 let select = document.querySelector('select')
@@ -69,9 +79,9 @@ let search = document.querySelector('input[type="search"]')
 let searchResult;
 search.addEventListener("input", function() {
     searchResult = search.value;
-})
+});
 wordSearch.addEventListener("submit", function() {
     event.preventDefault();  
-    removePhotos();
-    getPhotos(searchResult);
-})
+    removePhotos();    
+    getPhotos(searchResult);        
+});
--- script.js
+++ script.js
@@ -28,49 +28,32 @@ document.addEventListener('click', function(event) {
     }    
 });
 
-// WEATHER 
-let weatherRequest = new XMLHttpRequest();
-let url = new URL('http://api.openweathermap.org/data/2.5/weather');
-url.searchParams.set('lat', '59.93');
-url.searchParams.set('lon', '30.31');
-url.searchParams.set('appid', '8531afc05368e7ed4171872b5b9c8b7f');
-url.searchParams.set('units', 'metric');
-url.searchParams.set('lang', 'ru');
-weatherRequest.open('GET', url, true);
-weatherRequest.send();
-
-let weatherBlock = document.querySelector('.weather');
-
-weatherRequest.onload = function() {    
-    let response = JSON.parse(this.response);
-    let weatherData = response.main;
-
-    //Icon
-    let weatherInfo = response.weather[0];
-    let iconData = weatherInfo.icon;
-    let icon = document.createElement('img');
-    icon.src = `http://openweathermap.org/img/wn/${iconData}.png`;
-    weatherBlock.appendChild(icon);
-
-    //Temperature
-    let temperatureData =  weatherData.temp;
-    temperatureData = Math.round(temperatureData);
-    let temp = document.createElement('p');
-    if (temperatureData > 0) {
-        temp.textContent = " + " + temperatureData + " ℃";
-    } else {
-        temp.textContent = temperatureData + " ℃";
-    }         
-    temp.classList.add('degrees'); 
-    weatherBlock.appendChild(temp);
+// CLOCK
+let clockContainer = document.querySelector('.clock');
+function updateClock() {
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours < 10) hours = "0" + hours;
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = "0" + minutes;
+    let seconds = date.getSeconds();    
+    if (seconds < 10) seconds = "0" + hours; 
+    clockContainer.querySelector('.time').innerHTML = hours + ':' + minutes + ':' + seconds;
     
-    //Description
-    let descriptionData = weatherInfo.description;
-    createWeatherElement(descriptionData, 'p');
+// }
+
+let time;
+function startClock() {
+    time = setInterval(updateClock, 1000);
+    updateClock();
 }
-function createWeatherElement(element, tagName) {
-    let newElement = document.createElement(tagName);
-    newElement.textContent = element;
-    weatherBlock.appendChild(newElement);
+function stopClock() {
+    clearInterval(time);
 }
 
+startClock();
+
+let go = clockContainer.querySelector('.go');
+go.onclick = startClock;
+let stopCl = clockContainer.querySelector('.stop');
+stopCl.onclick = stopClock;
